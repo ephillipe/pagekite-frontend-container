c68367438a5473d302ef88fd42033088df82c68f
erickponce <erickponceleao@gmail.com>
2015-12-21 16:49:24 -0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker stop $(docker ps -q -f name=pagekite) && docker rm $(docker ps -a -q -f name=pagekite)
-docker build -t pagekite .
-docker run -itd --restart=always --name pagekite \
-           --volume /home/local/ALTERDATA/erickleao.dnt/dsn/web/page-kite/pagekite-frontend-container/.s3cfg:/.s3cfg \
-           -p 8080:80 pagekite
-
-
